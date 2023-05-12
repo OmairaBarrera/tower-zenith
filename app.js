d484fcade05af324e4b1adf1aa4f5cc402b6fc01
OmairaBarrera <lomaira.barrera@gmail.com>
2023-05-12 08:15:06 -0500

    feat: :sparkles: minor filter functionality
    the data of the recruits is filtered bringing only minors

--- app.js
+++ app.js
@@ -2,3 +2,17 @@ import myReclutas from "./components/myReclutas.js";
 
 let datareclutas = 'http://localhost:4500/reclutas';
 myReclutas.reculta(datareclutas);
+
+const botones = document.querySelector('.filtros')
+
+botones.addEventListener("click", e =>{
+    let btnMenores = document.querySelector('.btn-menores')
+    let btnMeses = document.querySelector('.btn-meses')
+    if(e.target === btnMenores){
+        console.log("menores");
+        let datareclutas = 'http://localhost:4500/reclutas?edad_lte=18';
+        myReclutas.reculta(datareclutas);
+    } else if(e.target === btnMeses){
+        console.log("meses");
+    }
+})
